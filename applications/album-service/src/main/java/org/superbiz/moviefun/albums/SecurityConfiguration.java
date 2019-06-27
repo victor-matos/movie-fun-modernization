6b2c887ab72cec05543834872e0aebfef27815e0
Victor Ferreira de Matos <victor.matos@furukawaelectric.com>
2019-06-27 11:26:09 -0600

    Security
    Providing security to a jsp app

---
+++ applications/album-service/src/main/java/org/superbiz/moviefun/albums/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package org.superbiz.moviefun.albums;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer
+@ConditionalOnProperty(value = "application.oauth.enabled", matchIfMissing = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().anyRequest().authenticated();
+    }
+}
